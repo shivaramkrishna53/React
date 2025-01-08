0838d8da316c095dfc879af5ca2b650bb6713689
shivaramkrishna53 <shivaramkrishna.krishna29@gmail.com>
2025-01-08 15:42:27 +0530

    UseReducer hook demo

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -1,9 +1,16 @@
-import DateCounter from "./DateCounter";
+// import DateCounter from "./DateCounter";
+import Header from "./Header";
+import Main from "./Main";
 
 export default function App() {
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      {/* <DateCounter /> */}
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
     </div>
   );
 }
